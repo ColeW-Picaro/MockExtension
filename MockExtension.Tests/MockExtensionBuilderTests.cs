a13d5d8f0698f931153973298d199fb53da04596
ColeW-Picaro <colevohs@gmail.com>
2024-09-16 22:32:33 -0400

    stand up tests

--- MockExtension.Tests/MockExtensionBuilderTests.cs
+++ MockExtension.Tests/MockExtensionBuilderTests.cs
@@ -10,14 +10,38 @@ public class MockExtensionBuilderTests
     }
 
     [Fact]
-    public void Test1()
+    public void NoArgs()
     {
-
         var methods = _mockExtensionBuilder.BuildMockExtensions<ITestInterface>();
         foreach (var method in methods)
         {
             Console.WriteLine(method);
         }
+    }
 
+    [Fact]
+    public void NoArgs_GeneratesExtension() {
+        var method = typeof(ITestInterface).GetMethod("NoArgs") ?? throw new Exception();
+        Assert.Equal(
+        _mockExtensionBuilder.GenerateMethod<ITestInterface>(method), 
+@"public static void WithNoArgs(this Mock<ITestInterface> mock, System.String returnType, Moq.Times times)
+{
+    mock.Setup(x => x.NoArgs())
+        .Returns(returnValue)
+        .Verifiable(times)
+}");
+    }
+    
+    [Fact]
+    public void NoArgsAsync_GeneratesExtension() {
+        var method = typeof(ITestInterface).GetMethod("NoArgsAsync") ?? throw new Exception();
+        Assert.Equal(
+        _mockExtensionBuilder.GenerateMethod<ITestInterface>(method), 
+@"public static void WithNoArgsAsync(this Mock<ITestInterface> mock, System.String returnType, Moq.Times times)
+{
+    mock.Setup(x => x.NoArgsAsync())
+        .ReturnsAsync(returnValue)
+        .Verifiable(times)
+}");
     }
 }
--- MockExtension/MockExtensionBuilder.cs
+++ MockExtension/MockExtensionBuilder.cs
@@ -6,38 +6,55 @@ namespace MockExtension;
 public class MockExtensionBuilder
 {
     public IEnumerable<string> BuildMockExtensions<T>()
-        where T: class
+        where T : class
+    {
+        Type type = ExtractType<T>();
+
+        foreach (var method in type.GetMethods())
+        {
+            string methodText = GenerateMethod<T>(method);
+            yield return methodText;
+        }
+    }
+
+    private static Type ExtractType<T>() where T : class
     {
         Type type = typeof(T);
         var targetInterface = type.IsInterface
             ? type
             : type.GetInterface($"I{type.Name}")
                 ?? throw new Exception("Could not find elegible interface for type");
-
-        foreach (var method in targetInterface.GetMethods())
-        {
-            var returnType = method.ReturnType;
-            var taskType = returnType.GenericTypeArguments.FirstOrDefault();
-            if (taskType != null) {
-                returnType = taskType;
-            }
-                
-            var thisParameter = new Parameter(typeof(Mock<T>), "mock");
-            var parameters = method.GetParameters();
-            string parameterList = string.Join(", ", parameters.Select(CreateParameter)
-                .Prepend(thisParameter)
-                .Append(new Parameter(returnType, "returnType"))
-                .Append(new Parameter(typeof(Moq.Times), "times")));
-            var methodText = 
-$@"public static void With{method.Name}({parameterList})
-{{
-    mock.Setup(x => x.{method.Name}())
-        .Returns{(taskType != null ? "Async" : "")}(returnValue)
-        .Verifiable(times)
-}}";
-            yield return methodText;
-        }
+        return targetInterface;
     }
 
-    private Parameter CreateParameter(ParameterInfo parameterInfo) => new Parameter(parameterInfo.GetType(), parameterInfo.Name);
+    public string GenerateMethod<T>(MethodInfo method)
+        where T : class
+    {
+        var type = ExtractType<T>();
+        var returnType = method.ReturnType;
+        var isAsync = false;
+        var taskType = returnType.GenericTypeArguments.FirstOrDefault();
+        if (taskType != null)
+        {
+            isAsync = true;
+            returnType = taskType;
+        }
+
+        var parameters = method.GetParameters();
+        var parameterList = string.Join(", ", parameters.Select(CreateParameter)
+            .Append(new Parameter(isAsync ? taskType! : returnType, "returnType"))
+            .Append(new Parameter(typeof(Moq.Times), "times")));
+        var methodText =
+$@"public static void With{method.Name}(this Mock<{type.Name}> mock, {parameterList})
+{{
+    mock.Setup(x => x.{method.Name}())
+        .Returns{(isAsync ? "Async" : "")}(returnValue)
+        .Verifiable(times)
+}}";
+        return methodText;
+    }
+
+    private Parameter CreateParameter(ParameterInfo parameterInfo)
+        => new Parameter(parameterInfo.GetType(),
+            parameterInfo.Name ?? throw new Exception());
 }
